8f216648c9f14fc206fb72a225aaa2a9fd51de8f
Praveen Perumal <ppraveen98841@gmail.com>
2022-04-21 16:07:58 +0530

    styled todofilter and footer

--- src/App.svelte
+++ src/App.svelte
@@ -17,11 +17,19 @@
 	:global(body) {
 		min-height: 100vh;
 		background-image: url('../images/bg-mobile-light.jpg');
+		background-color:	var(--theme-100); 
 		background-repeat: no-repeat;
 		background-size: contain;
 	}
 
 	:global(html) {
 		font-family: "Josefin Sans";
+	}
+
+	footer {
+		color: var(--theme-400);
+		font-size: var(--fn-200);
+		margin: 2.95em 0px;
+		text-align: center;
 	}
 </style>
--- src/components/TodoFilter.svelte
+++ src/components/TodoFilter.svelte
@@ -1,7 +1,57 @@
+<script>
+  let currentFilter = 0
+  let filters = [
+    {
+      name: 'All',
+      fn: (todo) => true
+    },
+    {
+      name: 'Active',
+      fn: (todo) => !todo.checked
+    },
+    {
+      name: 'Completed',
+      fn: (todo) => todo.checked
+    }
+  ]
+</script>
+
 <section class="todo-filter">
   <ul>
-    <li>All</li>
-    <li>Active</li>
-    <li>Completed</li>
+    {#each filters as filter,index (index)}
+      <li class:active="{currentFilter == index}" on:click="{() => {}}">
+        {filter.name}
+      </li>
+    {/each}
   </ul>
 </section>
+
+<style lang="scss">
+  section.todo-filter {
+    width: min(87.2vw, 540px);
+    min-height: 3em;
+    margin: 0px auto;
+    padding: 0px;
+  }
+
+  ul {
+    background-color: white;
+    border-radius: 0.3125em;
+    box-shadow: rgba(194, 195, 214, 0.5) 0px 2em 3.125em -1em;
+    min-height: 3em;
+    display: flex;
+    gap: 1.25em;
+    align-items: center;
+    justify-content: center;
+    list-style: none;
+  }
+
+  li {
+    color: var(--theme-400);
+    transition: color 400ms linear 0s;
+
+    &.active {
+      color: var(--clr-primary-blue);
+    }
+  }
+</style>
