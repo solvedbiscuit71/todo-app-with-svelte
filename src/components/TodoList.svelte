d7708444939eb6ee8bfe6886a6b58fbbfa35790a
Praveen Perumal <ppraveen98841@gmail.com>
2022-04-21 18:26:28 +0530

    complete checked functionality

--- src/components/TodoList.svelte
+++ src/components/TodoList.svelte
@@ -1,12 +1,22 @@
 <script>
   import { todos } from "../stores/todos";
+
+  function toggleCheck(id) {
+    const newTodos = [...$todos]
+    newTodos.forEach(todo => {
+      if (id === todo.id) {
+        todo.checked = todo.checked ? false : true
+      }
+    })
+    $todos = newTodos
+  }
 </script>
 
 <section class="todo-list">
   <ul>
     {#each $todos as todo (todo.id)}
       <li draggable="true">
-        <span class="checkbox" class:checked='{todo.checked}'></span>
+        <span class="checkbox" class:checked='{todo.checked}' on:click="{toggleCheck(todo.id)}"></span>
         <span class="text" class:checked='{todo.checked}'>{todo.text}</span>
         <svg class="close" xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path fill="#494C6B" fill-rule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"/></svg>
       </li>
